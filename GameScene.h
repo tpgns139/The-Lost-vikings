142ec35108a5764530e20dfa0c0fef98203a7fc4
sonjaeheon <sohn5127@gmail.com>
2019-12-27 16:12:09 +0900

    no message

--- GameScene.h
+++ GameScene.h
@@ -1,12 +1,16 @@
 #pragma once
 #include"gameNode.h"
 #include"PlayerManager.h"
+#include "Erik.h"
+
 class GameScene:public gameNode
 {
 private:
 	int count;
 	PlayerManager* _playerManager;
 	int currentX;
+
+	Erik* _erik;
 
 public:
 	
